ffdd37533f62b09ed20dee586524658dfb88c2c7
krauthaufen <krauthaufen@awx.at>
2015-03-03 11:05:30 +0100

    import

---
+++ DevILSharp/IL.fs
@@ -0,0 +1,338 @@
+﻿namespace DevILSharp
+
+module IL =
+    open System.Runtime.InteropServices
+    open System.Runtime.CompilerServices
+
+    [<Literal>]
+    let lib = "DevIL.dll"
+
+
+//    type ILenum = int
+
+    /// <summary>
+    /// Sets the active image to a frame in an animation.
+    /// </summary>
+    [<DllImport(lib, EntryPoint="ilActiveFace")>]
+    extern bool ActiveFace(int Number);
+
+    [<DllImport(lib, EntryPoint="ilActiveImage")>]
+    extern bool ActiveImage(int Number);
+
+    [<DllImport(lib, EntryPoint="ilActiveLayer")>]
+    extern bool ActiveLayer(int Number);
+
+    [<DllImport(lib, EntryPoint="ilActiveMipmap")>]
+    extern bool ActiveMipmap(int Number);
+
+    [<DllImport(lib, EntryPoint="ilApplyPal")>]
+    extern bool ApplyPal(string FileName);
+
+    [<DllImport(lib, EntryPoint="ilApplyProfile")>]
+    extern bool ApplyProfile(string InProfile, string OutProfile);
+
+    [<DllImport(lib, EntryPoint="ilBindImage")>]
+    extern void BindImage(int Image);
+
+    [<DllImport(lib, EntryPoint="ilBlit")>]
+    extern bool Blit(int Source, int DestX, int DestY, int DestZ, int SrcX, int SrcY, int SrcZ, int Width, int Height, int Depth);
+
+    [<DllImport(lib, EntryPoint="ilClampNTSC")>]
+    extern bool ClampNTSC();
+
+    [<DllImport(lib, EntryPoint="ilClearColour")>]
+    extern void ClearColour(float32 Red, float32 Green, float32 Blue, float32 Alpha);
+
+    [<DllImport(lib, EntryPoint="ilClearImage")>]
+    extern bool ClearImage();
+
+//    [<DllImport(lib)>]
+
+    [<DllImport(lib, EntryPoint="ilCloneCurImage")>]
+    extern int CloneCurImage();
+
+    [<DllImport(lib, EntryPoint="ilCompressDXT")>]
+    extern nativeint CompressDXT(nativeint Data, int Width, int Height, int Depth, CompressedDataFormat DXTCFormat, int* DXTCSize);
+
+    [<DllImport(lib, EntryPoint="ilCompressFunc")>]
+    extern bool CompressFunc(CompressMode Mode);
+
+    [<DllImport(lib, EntryPoint="ilConvertImage")>]
+    extern bool ConvertImage(Format DestFormat, ChannelType DestType);
+
+    [<DllImport(lib, EntryPoint="ilConvertPal")>]
+    extern bool ConvertPal(PalletteType DestFormat);
+
+    [<DllImport(lib, EntryPoint="ilCopyImage")>]
+    extern bool CopyImage(int Src);
+
+    [<DllImport(lib, EntryPoint="ilCopyPixels")>]
+    extern int CopyPixels(int XOff, int YOff, int ZOff, int Width, int Height, int Depth, Format Format, ChannelType Type, void *Data);
+
+    [<DllImport(lib, EntryPoint="ilCreateSubImage")>]
+    extern int CreateSubImage(SubImageType Type, int Num);
+
+    [<DllImport(lib, EntryPoint="ilDefaultImage")>]
+    extern bool DefaultImage();
+
+    [<DllImport(lib, EntryPoint="ilDeleteImage")>]
+    extern void DeleteImage(int Num);
+
+    [<DllImport(lib, EntryPoint="ilDeleteImages")>]
+    extern void DeleteImages(nativeint Num, int *Images);
+
+    [<DllImport(lib, EntryPoint="ilDetermineType")>]
+    extern ImageType DetermineType(string FileName);
+
+//    [<DllImport(lib)>]
+//    [<DllImport(lib, EntryPoint="ilDetermineTypeF")>]
+//    extern ILenum DetermineTypeF(ILHANDLE File);
+
+    [<DllImport(lib, EntryPoint="ilDetermineTypeL")>]
+    extern ImageType DetermineTypeL(void *Lump, int Size);
+
+    [<DllImport(lib, EntryPoint="ilDisable")>]
+    extern bool Disable(EnableCap Mode);
+
+    [<DllImport(lib, EntryPoint="ilDxtcDataToImage")>]
+    extern bool DxtcDataToImage();
+
+    [<DllImport(lib, EntryPoint="ilDxtcDataToSurface")>]
+    extern bool DxtcDataToSurface();
+
+    [<DllImport(lib, EntryPoint="ilEnable")>]
+    extern bool Enable(EnableCap Mode);
+
+    [<DllImport(lib, EntryPoint="ilFlipSurfaceDxtcData")>]
+    extern void FlipSurfaceDxtcData();
+
+    [<DllImport(lib, EntryPoint="ilFormatFunc")>]
+    extern bool FormatFunc(Format Mode);
+
+    [<DllImport(lib, EntryPoint="ilGenImages")>]
+    extern void GenImages(nativeint Num, int *Images);
+
+    [<DllImport(lib, EntryPoint="ilGenImage")>]
+    extern int GenImage();
+
+    [<DllImport(lib, EntryPoint="ilGetAlpha")>]
+    extern nativeint GetAlpha(ChannelType Type);
+
+    [<DllImport(lib, EntryPoint="ilGetBoolean")>]
+    extern bool GetBoolean(GetBooleanName Mode);
+
+    [<DllImport(lib, EntryPoint="ilGetBooleanv")>]
+    extern void GetBooleanv(GetBooleanName Mode, bool *Param);
+    
+    [<DllImport(lib, EntryPoint="ilGetData")>]
+    extern nativeint GetData();
+
+    [<DllImport(lib, EntryPoint="ilGetDXTCData")>]
+    extern int GetDXTCData(void *Buffer, int BufferSize, CompressedDataFormat DXTCFormat);
+
+    [<DllImport(lib, EntryPoint="ilGetError")>]
+    extern ErrorCode GetError();
+
+    [<DllImport(lib, EntryPoint="ilGetInteger")>]
+    extern int GetInteger(GetName Mode);
+
+    [<DllImport(lib, EntryPoint="ilGetIntegerv")>]
+    extern void GetIntegerv(GetName Mode, int *Param);
+
+    let GetFormat() =
+        GetInteger(GetName.ImageFormat) |> unbox<Format>
+
+    let GetDataType() =
+        GetInteger(GetName.ImageType) |> unbox<ChannelType>
+
+    [<DllImport(lib, EntryPoint="ilGetLumpPos")>]
+    extern int GetLumpPos();
+    
+    [<DllImport(lib, EntryPoint="ilGetPalette")>]
+    extern nativeint GetPalette();
+
+    [<DllImport(lib, EntryPoint="ilGetString")>]
+    extern string GetString(GetStringName StringName);
+
+//    [<DllImport(lib, EntryPoint="ilHint")>]
+//    extern void Hint(ILenum Target, ILenum Mode);
+
+    [<DllImport(lib, EntryPoint="ilInvertSurfaceDxtcDataAlpha")>]
+    extern bool InvertSurfaceDxtcDataAlpha();
+
+    [<DllImport(lib, EntryPoint="ilInit")>]
+    extern void Init();
+
+    [<DllImport(lib, EntryPoint="ilImageToDxtcData")>]
+    extern bool ImageToDxtcData(CompressedDataFormat Format);
+
+    [<DllImport(lib, EntryPoint="ilIsDisabled")>]
+    extern bool IsDisabled(EnableCap Mode);
+
+    [<DllImport(lib, EntryPoint="ilIsEnabled")>]
+    extern bool IsEnabled(EnableCap Mode);
+
+    [<DllImport(lib, EntryPoint="ilIsImage")>]
+    extern bool IsImage(int Image);
+
+    [<DllImport(lib, EntryPoint="ilIsValid")>]
+    extern bool IsValid(ImageType Type, string FileName);
+
+//    [<DllImport(lib, EntryPoint="ilIsValidF")>]
+//    extern bool IsValidF(ILenum Type, ILHANDLE File);
+
+    [<DllImport(lib, EntryPoint="ilIsValidL")>]
+    extern bool IsValidL(ImageType Type, void *Lump, int Size);
+
+    [<DllImport(lib, EntryPoint="ilKeyColour")>]
+    extern void KeyColour(float32 Red, float32 Green, float32 Blue, float32 Alpha);
+
+    [<DllImport(lib, EntryPoint="ilLoad")>]
+    extern bool Load(ImageType Type, string FileName);
+
+//    [<DllImport(lib, EntryPoint="ilLoadF")>]
+//    extern bool LoadF(ILenum Type, ILHANDLE File);
+
+    [<DllImport(lib, EntryPoint="ilLoadImage")>]
+    extern bool LoadImage(string FileName);
+
+    [<DllImport(lib, EntryPoint="ilLoadL")>]
+    extern bool LoadL(ImageType Type, void *Lump, int Size);
+
+    [<DllImport(lib, EntryPoint="ilLoadPal")>]
+    extern bool LoadPal(string FileName);
+
+    [<DllImport(lib, EntryPoint="ilModAlpha")>]
+    extern void ModAlpha(float AlphaValue);
+
+    [<DllImport(lib, EntryPoint="ilOriginFunc")>]
+    extern bool OriginFunc(OriginMode Mode);
+
+    [<DllImport(lib, EntryPoint="ilOverlayImage")>]
+    extern bool OverlayImage(int Source, int XCoord, int YCoord, int ZCoord);
+
+    [<DllImport(lib, EntryPoint="ilPopAttrib")>]
+    extern void PopAttrib();
+
+    [<DllImport(lib, EntryPoint="ilPushAttrib")>]
+    extern void PushAttrib(int Bits);
+
+    [<DllImport(lib, EntryPoint="ilRegisterFormat")>]
+    extern void RegisterFormat(Format Format);
+
+//    [<DllImport(lib, EntryPoint="ilRegisterLoad")>]
+//    extern bool RegisterLoad(string Ext, IL_LOADPROC Load);
+
+    [<DllImport(lib, EntryPoint="ilRegisterMipNum")>]
+    extern bool RegisterMipNum(int Num);
+
+    [<DllImport(lib, EntryPoint="ilRegisterNumFaces")>]
+    extern bool RegisterNumFaces(int Num);
+
+    [<DllImport(lib, EntryPoint="ilRegisterNumImages")>]
+    extern bool RegisterNumImages(int Num);
+
+    [<DllImport(lib, EntryPoint="ilRegisterOrigin")>]
+    extern void RegisterOrigin(OriginMode Origin);
+
+    [<DllImport(lib, EntryPoint="ilRegisterPal")>]
+    extern void RegisterPal(void *Pal, int Size, PalletteType Type);
+
+//    [<DllImport(lib, EntryPoint="ilRegisterSave")>]
+//    extern bool RegisterSave(string Ext, IL_SAVEPROC Save);
+
+    [<DllImport(lib, EntryPoint="ilRegisterType")>]
+    extern void RegisterType(ChannelType Type);
+
+    [<DllImport(lib, EntryPoint="ilRemoveLoad")>]
+    extern bool RemoveLoad(string Ext);
+
+    [<DllImport(lib, EntryPoint="ilRemoveSave")>]
+    extern bool RemoveSave(string Ext);
+
+    [<DllImport(lib, EntryPoint="ilResetMemory")>]
+    extern void ResetMemory(); // Deprecated
+
+    [<DllImport(lib, EntryPoint="ilResetRead")>]
+    extern void ResetRead();
+
+    [<DllImport(lib, EntryPoint="ilResetWrite")>]
+    extern void ResetWrite();
+
+    [<DllImport(lib, EntryPoint="ilSave")>]
+    extern bool Save(ImageType Type, string FileName);
+
+//    [<DllImport(lib, EntryPoint="ilSaveF")>]
+//    extern int SaveF(ILenum Type, ILHANDLE File);
+
+    [<DllImport(lib, EntryPoint="ilSaveImage")>]
+    extern bool SaveImage(string FileName);
+
+    [<DllImport(lib, EntryPoint="ilSaveL")>]
+    extern int SaveL(ImageType Type, void *Lump, int Size);
+
+    [<DllImport(lib, EntryPoint="ilSavePal")>]
+    extern bool SavePal(string FileName);
+
+    [<DllImport(lib, EntryPoint="ilSetAlpha")>]
+    extern bool SetAlpha(float AlphaValue);
+
+    [<DllImport(lib, EntryPoint="ilSetData")>]
+    extern bool SetData(void *Data);
+
+    [<DllImport(lib, EntryPoint="ilSetDuration")>]
+    extern bool SetDuration(int Duration);
+
+    [<DllImport(lib, EntryPoint="ilSetInteger")>]
+    extern void SetInteger(GetName Mode, int Param);
+
+//    [<DllImport(lib, EntryPoint="ilSetMemory")>]
+//    extern void SetMemory(mAlloc, mFree);
+
+    [<DllImport(lib, EntryPoint="ilSetPixels")>]
+    extern void SetPixels(int XOff, int YOff, int ZOff, int Width, int Height, int Depth, Format Format, ChannelType Type, void *Data);
+
+//    [<DllImport(lib, EntryPoint="ilSetRead")>]
+//    extern void SetRead(fOpenRProc, fCloseRProc, fEofProc, fGetcProc, fReadProc, fSeekRProc, fTellRProc);
+
+    [<DllImport(lib, EntryPoint="ilSetString")>]
+    extern void SetString(GetStringName Mode,  string String);
+
+//    [<DllImport(lib, EntryPoint="ilSetWrite")>]
+//    extern void SetWrite(fOpenWProc, fCloseWProc, fPutcProc, fSeekWProc, fTellWProc, fWriteProc);
+
+    [<DllImport(lib, EntryPoint="ilShutDown")>]
+    extern void ShutDown();
+
+    [<DllImport(lib, EntryPoint="ilSurfaceToDxtcData")>]
+    extern bool SurfaceToDxtcData(CompressedDataFormat Format);
+
+    [<DllImport(lib, EntryPoint="ilTexImage")>]
+    extern bool TexImage(int Width, int Height, int Depth, byte NumChannels, Format Format, ChannelType Type, void *Data);
+
+    [<DllImport(lib, EntryPoint="ilTexImageDxtc")>]
+    extern bool TexImageDxtc(int w, int h, int d, CompressedDataFormat DxtFormat, nativeint data);
+
+    [<DllImport(lib, EntryPoint="ilTypeFromExt")>]
+    extern ImageType TypeFromExt(string FileName);
+
+//    [<DllImport(lib, EntryPoint="ilTypeFunc")>]
+//    extern bool TypeFunc(ILenum Mode);
+
+    [<DllImport(lib, EntryPoint="ilLoadData")>]
+    extern bool LoadData(string FileName, int Width, int Height, int Depth, byte Bpp);
+
+//    [<DllImport(lib, EntryPoint="ilLoadDataF")>]
+//    extern bool LoadDataF(ILHANDLE File, int Width, int Height, int Depth, byte Bpp);
+
+    [<DllImport(lib, EntryPoint="ilLoadDataL")>]
+    extern bool LoadDataL(void *Lump, int Size, int Width, int Height, int Depth, byte Bpp);
+
+    [<DllImport(lib, EntryPoint="ilSaveData")>]
+    extern bool SaveData(string FileName);
+
+
+    let check (error : string) (v : bool) =
+        if not v then
+            let err = GetError()
+            printfn "ERROR %A: %s" err error
---
+++ DevILSharp/Program.fs
@@ -0,0 +1,57 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+
+open System
+open System.Runtime.InteropServices
+open DevILSharp
+
+[<EntryPoint>]
+let main argv = 
+    IL.Init()
+    IL.Enable(EnableCap.OverwriteExistingFile) |> IL.check "could enable overwrite"
+    IL.Enable(EnableCap.AbsoluteOrigin) |> IL.check "could enable absolute origin"
+    let img = IL.GenImage()
+
+    IL.BindImage(img)
+    IL.LoadImage(@"C:\Users\Schorsch\Desktop\SixteenBitRGB.tif") |> IL.check "could not load image"
+
+    let ptr = IL.GetData()
+    let size = IL.GetInteger(GetName.ImageSizeOfData)
+    let format = IL.GetFormat()
+    let pixelType = IL.GetDataType()
+    let width = IL.GetInteger(GetName.ImageWidth)
+    let height = IL.GetInteger(GetName.ImageHeight)
+
+
+    let arr : byte[] = Array.zeroCreate size
+    Marshal.Copy(ptr, arr, 0, arr.Length)
+    IL.BindImage(0)
+    IL.DeleteImage(img)
+
+
+    let img = IL.GenImage()
+    IL.BindImage(img)
+    let gc = GCHandle.Alloc(arr, GCHandleType.Pinned)
+
+    IL.TexImage(width, height, 1, 3uy, Format.RGB, ChannelType.UnsignedShort, gc.AddrOfPinnedObject()) |> IL.check "could not set image data"
+    IL.ConvertImage(Format.RGB, ChannelType.UnsignedByte) |> IL.check "could not convert image"
+    
+    IL.SaveImage("C:\\Users\\schorsch\\Desktop\\test.png") |> IL.check "could not save image"
+
+    gc.Free()
+    IL.BindImage(0)
+    IL.DeleteImage(img)
+
+
+
+
+
+    printfn "%A" arr
+
+    IL.ShutDown()
+
+
+
+
+    printfn "%A" argv
+    0 // return an integer exit code
