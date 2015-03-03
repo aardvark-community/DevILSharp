be2d4d12dd4177e93368884fdda5ff8f40a7bd57
haaser <haaser@vrvis.at>
2015-03-03 13:29:04 +0100

    added nuget packing to build script;

--- build.fsx
+++ build.fsx
@@ -27,6 +27,61 @@ Target "Projects" (fun () ->
 )
 
 
+
+Target "CreatePackage" (fun () ->
+    let branch = Fake.Git.Information.getBranchName "."
+    let releaseNotes = Fake.Git.Information.getCurrentHash()
+
+    if branch = "master" then
+        let tag = Fake.Git.Information.getLastTag()
+        NuGetPack (fun p -> 
+            { p with OutputPath = "bin"; 
+                     Version = tag; 
+                     ReleaseNotes = releaseNotes; 
+                     WorkingDir = "bin"
+            }) "bin/DevILSharp.nuspec"
+    
+    else 
+        traceError (sprintf "cannot create package for branch: %A" branch)
+)
+
+Target "Deploy" (fun () ->
+
+    let accessKeyPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".ssh", "nuget.key")
+    let accessKey =
+        if File.Exists accessKeyPath then Some (File.ReadAllText accessKeyPath)
+        else None
+
+    let branch = Fake.Git.Information.getBranchName "."
+    let releaseNotes = Fake.Git.Information.getCurrentHash()
+    if branch = "master" then
+        let tag = Fake.Git.Information.getLastTag()
+        match accessKey with
+            | Some accessKey ->
+                try
+                    NuGetPublish (fun p -> 
+                        { p with 
+                            Project = "DevILSharp"
+                            OutputPath = "bin"
+                            Version = tag; 
+                            ReleaseNotes = releaseNotes; 
+                            WorkingDir = "bin"
+                            AccessKey = accessKey
+                            Publish = true
+                        })
+                with e ->
+                    ()
+            | None ->
+                ()
+     else 
+        traceError (sprintf "cannot deploy branch: %A" branch)
+)
+
+
+"Projects" ==>
+    "CreatePackage" ==>
+    "Deploy"
+
 // start build
 RunTargetOrDefault "Projects"
 
