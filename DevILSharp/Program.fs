d1463abf823db0263990d0345dce7ed867c83f95
haaser <haaser@vrvis.at>
2015-03-03 13:23:47 +0100

    DevILSharp is now a class library;

--- DevILSharp/Program.fs
+++ DevILSharp/Program.fs
@@ -1,61 +1,55 @@
-﻿// Learn more about F# at http://fsharp.net
-// See the 'F# Tutorial' project for more help.
+﻿namespace DevILSharp.Demo
+
 
 open System
 open System.Runtime.InteropServices
 open DevILSharp
 
-[<EntryPoint>]
-let main argv = 
-    Bootstrap.Init()
-    IL.Init()
-    IL.Enable(EnableCap.OverwriteExistingFile) |> IL.check "could enable overwrite"
-    IL.Enable(EnableCap.AbsoluteOrigin) |> IL.check "could enable absolute origin"
-    let img = IL.GenImage()
+module Demo = 
+    let run() = 
+        Bootstrap.Init()
+        IL.Init()
+        IL.Enable(EnableCap.OverwriteExistingFile) |> IL.check "could enable overwrite"
+        IL.Enable(EnableCap.AbsoluteOrigin) |> IL.check "could enable absolute origin"
+        let img = IL.GenImage()
 
-    IL.BindImage(img)
-    use s = new System.IO.FileStream(@"C:\Users\haaser\Desktop\SixteenBitRGB.tif", IO.FileMode.Open)
-    IL.LoadStream(ImageType.Tif, s) |> IL.check "could not load image"
+        IL.BindImage(img)
+        use s = new System.IO.FileStream(@"C:\Users\haaser\Desktop\SixteenBitRGB.tif", IO.FileMode.Open)
+        IL.LoadStream(ImageType.Tif, s) |> IL.check "could not load image"
 
-    let ptr = IL.GetData()
-    let size = IL.GetInteger(GetName.ImageSizeOfData)
-    let format = IL.GetFormat()
-    let pixelType = IL.GetDataType()
-    let width = IL.GetInteger(GetName.ImageWidth)
-    let height = IL.GetInteger(GetName.ImageHeight)
+        let ptr = IL.GetData()
+        let size = IL.GetInteger(GetName.ImageSizeOfData)
+        let format = IL.GetFormat()
+        let pixelType = IL.GetDataType()
+        let width = IL.GetInteger(GetName.ImageWidth)
+        let height = IL.GetInteger(GetName.ImageHeight)
 
 
     
-    let arr : byte[] = Array.zeroCreate size
-    Marshal.Copy(ptr, arr, 0, arr.Length)
-    IL.BindImage(0)
-    IL.DeleteImage(img)
+        let arr : byte[] = Array.zeroCreate size
+        Marshal.Copy(ptr, arr, 0, arr.Length)
+        IL.BindImage(0)
+        IL.DeleteImage(img)
 
 
-    let img = IL.GenImage()
-    IL.BindImage(img)
-    let gc = GCHandle.Alloc(arr, GCHandleType.Pinned)
+        let img = IL.GenImage()
+        IL.BindImage(img)
+        let gc = GCHandle.Alloc(arr, GCHandleType.Pinned)
 
-    IL.TexImage(width, height, 1, 3uy, Format.RGB, ChannelType.UnsignedShort, gc.AddrOfPinnedObject()) |> IL.check "could not set image data"
-    IL.ConvertImage(Format.RGB, ChannelType.UnsignedByte) |> IL.check "could not convert image"
+        IL.TexImage(width, height, 1, 3uy, Format.RGB, ChannelType.UnsignedShort, gc.AddrOfPinnedObject()) |> IL.check "could not set image data"
+        IL.ConvertImage(Format.RGB, ChannelType.UnsignedByte) |> IL.check "could not convert image"
     
-    IL.SaveImage("C:\\Users\\haaser\\Desktop\\test.png") |> IL.check "could not save image"
+        IL.SaveImage("C:\\Users\\haaser\\Desktop\\test.png") |> IL.check "could not save image"
 
-    gc.Free()
-    IL.BindImage(0)
-    IL.DeleteImage(img)
-
-
-
-
-
-    printfn "%A" arr
-
-    IL.ShutDown()
+        gc.Free()
+        IL.BindImage(0)
+        IL.DeleteImage(img)
 
 
 
 
-    printfn "%A" argv
-    0 // return an integer exit code
+
+        printfn "%A" arr
+
+        IL.ShutDown()
 
