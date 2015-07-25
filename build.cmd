f0f6bbb5a2a617815542d92173b46191fed07609
krauthaufen <krauthaufen@awx.at>
2015-07-25 14:03:08 +0200

    fixed build script and nuspec;

--- build.cmd
+++ build.cmd
@@ -6,8 +6,8 @@ cls
 
 IF exist packages\FAKE ( echo skipping FAKE download ) ELSE ( 
 echo downloading FAKE
-"bin\nuget.exe" "install" "FAKE" "-OutputDirectory" "packages" "-ExcludeVersion" "-Prerelease"
-"bin\nuget.exe" "install" "FSharp.Formatting.CommandTool" "-OutputDirectory" "packages" "-ExcludeVersion" "-Prerelease"
+"bin\nuget.exe" "install" "FAKE" "-OutputDirectory" "packages" "-ExcludeVersion" 
+"bin\nuget.exe" "install" "FSharp.Formatting.CommandTool" "-OutputDirectory" "packages" "-ExcludeVersion"
 "bin\nuget.exe" "install" "SourceLink.Fake" "-OutputDirectory" "packages" "-ExcludeVersion"
 "bin\nuget.exe" "install" "NUnit.Runners" "-OutputDirectory" "packages" "-ExcludeVersion"
 )
