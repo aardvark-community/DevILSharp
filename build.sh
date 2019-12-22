710a6b34b014ccd3c7001406497277925a2df31d
Stefan Maierhofer <sm@stefanmaierhofer.com>
2019-12-22 11:56:58 +0100

    Revert "update packages, fsharp.core 4.7.0"
    This reverts commit 2f4359c3022f0ac72e6a9b2a7e76cf9077c44223.

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-if [ ! -f .paket/paket ]; then
-    dotnet tool install Paket --tool-path .paket
-fi
+mono .paket/paket.bootstrapper.exe
+mono .paket/paket.exe restore --group Build
 
-./.paket/paket restore 
 
-dotnet packages/build/fake-cli/tools/netcoreapp2.1/any/fake-cli.dll build $@
+#mono packages/build/FAKE/tools/FAKE.exe "build.fsx" Dummy --fsiargs build.fsx $@
+echo "currently our build script does not work on mono >= 5.0. for that reason we use vanilla builds (which makes addsource etc unavailable in that form. you need to use fsi sessions for portions of the build script which fork. please do so and create issues describing the mono/fsharp issues)"
+mono .paket/paket.exe install
+msbuild src/Aardvark.sln
