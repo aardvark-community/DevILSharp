4ccdcb318070cc79ab0569d7a1d0ce16be2865d5
krauthaufen <krauthaufen@awx.at>
2020-02-20 20:20:18 +0100

    added macos libs and updated build script

--- build.sh
+++ build.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-mono .paket/paket.bootstrapper.exe
-mono .paket/paket.exe restore --group Build
+if [ ! -f .paket/paket ]; then
+    dotnet tool install Paket --tool-path .paket
+fi
 
+./.paket/paket restore 
 
-#mono packages/build/FAKE/tools/FAKE.exe "build.fsx" Dummy --fsiargs build.fsx $@
-echo "currently our build script does not work on mono >= 5.0. for that reason we use vanilla builds (which makes addsource etc unavailable in that form. you need to use fsi sessions for portions of the build script which fork. please do so and create issues describing the mono/fsharp issues)"
-mono .paket/paket.exe install
-msbuild src/Aardvark.sln
+dotnet packages/build/fake-cli/tools/netcoreapp2.1/any/fake-cli.dll build $@
